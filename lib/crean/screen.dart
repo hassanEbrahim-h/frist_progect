bf63c9a0e90e0118f6607114877bc6f02b9f78e5
hassan Ebrahim_h <hasanzao062@gmail.com>
2025-11-25 13:53:27 +0200

    Initial commit

--- lib/crean/screen.dart
+++ lib/crean/screen.dart
@@ -4,40 +4,41 @@ import 'package:flutter/material.dart';
 
 class crean extends StatelessWidget {
   const crean({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:  AppBar(
-        backgroundColor:Colors.blueGrey ,
-        title:Text("Computer Scines",style: TextStyle(
-          color: Colors.black,
-              fontSize: 30,
-          fontWeight: FontWeight.bold,
-        ), ) ,
+      appBar: AppBar( // 👈🏻 هنا تبدأ خاصية شريط العنوان
+        backgroundColor: Colors.blueGrey,
+        title: Text("Computer Scines"), // استخدم ويدجت النص مباشرة
         centerTitle: true,
+        leading: Icon(Icons.list, color: Colors.white), // 👈🏻 هنا يجب أن تكون خاصية leading
         actions: [
-          IconButton(onPressed: (){}, icon: Icon(Icons.search,color: Colors.white, size: 30,)),
-          IconButton(onPressed: (){}, icon: Icon(Icons.favorite_border,color: Colors.red, size: 30,))
-        ],
-        leading: Icon(Icons.list, color: Colors.white,),
-      ),
-      body: Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFIdo3usKWg1U787wTEh5wDvkRWxfuq_yeFA&s",
-        height:900,
+          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.favorite_border)),
+        ], // 👈🏻 نهاية actions
+      ), // 👈🏻 نهاية ويدجت AppBar
 
-      ),
-      floatingActionButton: FloatingActionButton(onPressed:(){},
-     child: Icon(Icons.add),
-      backgroundColor: Colors.indigoAccent,
-      ),
-      bottomNavigationBar: BottomNavigationBar(items:[
-        BottomNavigationBarItem(icon: Icon(Icons.home),label: "home"),
-        BottomNavigationBarItem(icon: Icon(Icons.favorite),label: "favorite")
-      ],
-      backgroundColor: Colors.brown,
+      body:
+         Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFIdo3usKWg1U787wTEh5wDvkRWxfuq_yeFA&s",
+           
+
+      ), // 👈🏻 نهاية الـ body
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.indigoAccent,
+      ), // 👈🏻 نهاية floatingActionButton
+
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "home"),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite), label: "favorite"),
+        ],
+        backgroundColor: Colors.brown,
         unselectedItemColor: Colors.white,
         selectedItemColor: Colors.black,
-      ),
-    );
+      ), // 👈🏻 نهاية bottomNavigationBar
+    ); // 👈🏻 نهاية الـ Scaffold
   }
 }
