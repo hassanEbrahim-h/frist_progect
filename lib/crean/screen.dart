b80fa866b2bc9294540ca136f7ae5dceeb11b96c
hassan Ebrahim_h <hasanzao062@gmail.com>
2025-11-25 14:23:46 +0200

    Initial commit

--- lib/crean/screen.dart
+++ lib/crean/screen.dart
@@ -9,19 +9,17 @@ class crean extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blueGrey,
-        title: Text("Computer Scines"),
+        title: Text("Computer Sicnces"),
         centerTitle: true,
         leading: Icon(Icons.list, color: Colors.white),
         actions: [
           IconButton(onPressed: () {}, icon: Icon(Icons.search)),
           IconButton(onPressed: () {}, icon: Icon(Icons.favorite_border)),
-        ]
+        ],
       ),
 
-      body:
-         Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFIdo3usKWg1U787wTEh5wDvkRWxfuq_yeFA&s",
-
-
+      body: Image.network(
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFIdo3usKWg1U787wTEh5wDvkRWxfuq_yeFA&s",
       ),
 
       floatingActionButton: FloatingActionButton(
@@ -33,12 +31,12 @@ class crean extends StatelessWidget {
       bottomNavigationBar: BottomNavigationBar(
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "home"),
-          BottomNavigationBarItem(icon: Icon(Icons.favorite), label: "favorite"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite), label: "favorite"),
         ],
         backgroundColor: Colors.brown,
-        unselectedItemColor: Colors.white,
         selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.white,
       ),
     );
-  }
-}
+  }}
